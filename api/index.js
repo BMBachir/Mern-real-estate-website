2dab19dae12c348238f6b9b63ec38cbd1bbf2cb2
BMBachir <boubaidjabachir3@gmail.com>
2024-04-13 22:42:06 +0100

    db connected

--- api/index.js
+++ api/index.js
@@ -3,7 +3,14 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO);
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB...");
+  })
+  .catch((err) => {
+    console.log("Error:" + err);
+  });
 
 const app = express();
 
