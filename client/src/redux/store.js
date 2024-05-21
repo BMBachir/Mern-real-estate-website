93db5ddc82aed3dbde01d925fd5122fd2f70dd46
BMBachir <boubaidjabachir3@gmail.com>
2024-05-21 16:16:40 +0100

    final installing redux toolkit

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./user/userSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    user: userReducer,
+  },
   middleware: (getDefaultMiddlware) => getDefaultMiddlware({}),
 });
