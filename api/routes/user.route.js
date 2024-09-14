f01c3b5f5bb0bb15ecfc33e4e4d600f0e959a9c2
BMBachir <boubaidjabachir3@gmail.com>
2024-09-14 18:54:34 +0100

    create the delete account API

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,9 +2,11 @@ import express from "express";
 import { test } from "../controllers/user.controller.js";
 import { updateUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyToken.js";
+import { deleteUser } from "../controllers/user.controller.js";
 const router = express.Router();
 
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
+router.delete("/delete/:id", verifyToken, deleteUser);
 
 export default router;
