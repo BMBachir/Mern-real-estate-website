60bce2317c71474285b9c7ea2ba5ae39d20e20eb
BMBachir <boubaidjabachir3@gmail.com>
2024-09-15 11:41:04 +0100

    adding the signout Api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -73,3 +73,12 @@ export const google = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signOut = async (req, res, next) => {
+  try {
+    res.clearCookie("access_token");
+    res.send(200).json("User has been logged out !!");
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { google, signin, signup } from "../controllers/auth.controller.js";
+import {
+  google,
+  signin,
+  signOut,
+  signup,
+} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/google", google);
+router.get("/signout", signOut);
 
 export default router;
