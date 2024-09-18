9b632430f455a413253dd9527d3553ed92613b5f
BMBachir <boubaidjabachir3@gmail.com>
2024-09-18 18:45:30 +0100

    fix an issu causing stop the server

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -8,9 +8,8 @@ import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
-router.get("/listing/:id", verifyToken, getUserListing);
+router.get("/listings/:id", verifyToken, getUserListing);
 
 export default router;
