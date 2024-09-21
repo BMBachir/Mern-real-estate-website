9a93d973666785eebcda6da964597ae5149205ae
BMBachir <boubaidjabachir3@gmail.com>
2024-09-21 16:00:38 +0100

    Delete user listing route and functionallity

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
   try {
@@ -6,5 +7,23 @@ export const createListing = async (req, res, next) => {
     return res.status(201).json(listing);
   } catch (error) {
     next(error);
+  }
+};
+
+export const deleteListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, "Listing not found"));
+    }
+    if (req.user.id !== listing.userRef.toString()) {
+      // Ensure IDs are of the same type (string comparison)
+      return next(errorHandler(401, "You can only delete your listings"));
+    }
+
+    await Listing.findByIdAndDelete(req.params.id);
+    return res.status(200).json({ message: "Listing deleted successfully" });
+  } catch (error) {
+    return next(error);
   }
 };
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyToken.js";
-import { createListing } from "../controllers/listing.controller.js";
+import {
+  createListing,
+  deleteListing,
+} from "../controllers/listing.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
+router.delete("/delete/:id", verifyToken, deleteListing);
 
 export default router;
