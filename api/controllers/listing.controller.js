d1b422ca0180705b0d1e2c348c256d854bc2d296
BMBachir <boubaidjabachir3@gmail.com>
2024-09-21 16:12:01 +0100

    adding Update api route and funcionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -27,3 +27,25 @@ export const deleteListing = async (req, res, next) => {
     return next(error);
   }
 };
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
+  if (req.user.id !== listing.userRef.toString()) {
+    // Ensure IDs are of the same type (string comparison)
+    return next(errorHandler(401, "You can only update your listings"));
+  }
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -3,11 +3,13 @@ import { verifyToken } from "../utils/verifyToken.js";
 import {
   createListing,
   deleteListing,
+  updateListing,
 } from "../controllers/listing.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
+router.post("/update/:id", verifyToken, updateListing);
 
 export default router;
