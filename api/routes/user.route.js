a9cceb243236c1a4571eb82c700bb1495d28c6fe
BMBachir <boubaidjabachir3@gmail.com>
2024-09-22 17:10:45 +0100

    get a user by id API

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,6 +3,7 @@ import {
   getUserListing,
   deleteUser,
   updateUser,
+  getUser,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyToken.js";
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 router.get("/listings/:id", verifyToken, getUserListing);
+router.get("/:id", verifyToken, getUser);
 
 export default router;
