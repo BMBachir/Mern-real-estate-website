e9b970ea91e11d89bf33deb6c01841bdc86d5dd9
BMBachir <boubaidjabachir3@gmail.com>
2024-09-28 22:18:37 +0100

    Set Content Security Policy

--- api/index.js
+++ api/index.js
@@ -37,10 +37,11 @@ app.use(helmet());
 app.use(morgan("dev"));
 
 // Set Content Security Policy
+
 app.use((req, res, next) => {
   res.setHeader(
     "Content-Security-Policy",
-    "default-src 'self'; img-src 'self' https://firebasestorage.googleapis.com;"
+    "default-src 'self'; script-src 'self' https://apis.google.com; img-src 'self' https://firebasestorage.googleapis.com;"
   );
   next();
 });
