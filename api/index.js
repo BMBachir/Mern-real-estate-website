319934b5fa1d7bdf8b03454b4b9b2947f47e0560
BMBachir <boubaidjabachir3@gmail.com>
2024-09-29 11:24:11 +0100

    Set Content Security Policy

--- api/index.js
+++ api/index.js
@@ -41,7 +41,7 @@ app.use(morgan("dev"));
 app.use((req, res, next) => {
   res.setHeader(
     "Content-Security-Policy",
-    "default-src 'self'; connect-src 'self' https://firebasestorage.googleapis.com; img-src 'self' https://firebasestorage.googleapis.com;"
+    "default-src 'self'; script-src 'self' https://apis.google.com; connect-src 'self' https://firebasestorage.googleapis.com; img-src 'self' https://firebasestorage.googleapis.com https://docs.material-tailwind.com;"
   );
   next();
 });
